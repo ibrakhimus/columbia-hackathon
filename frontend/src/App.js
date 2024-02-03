0962497c38569a5fce2afc0dbc4a766a8652eca2
ibrakhimus <randizzys@gmail.com>
2024-02-03 16:00:37 -0500

    react routing :)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from './pages/Landing';
+import Bills from './pages/Bills';
 
 function App() {
   return (
-    <div>
-      <Landing />
-    </div>
+    <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/bills" element={<Bills />} />
+      </Routes>
+    </Router>
+    </>
   );
 }
 
