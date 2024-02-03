fff8dd80aa6f34b7b5830bc1e940ab7c129e54fd
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-02-03 16:26:56 -0500

    added get_news api

--- backend/server.py
+++ backend/server.py
@@ -2,6 +2,7 @@ import requests
 import json
 from flask import Flask
 from image_search import image_search
+from world_news import get_news
 
 HEADERS = {'X-API-Key': 'flXU8LPnz82pSjKUSQEWWQd4YfpKuLfDGe9DXw50'}
 BASEURL = "https://api.propublica.org/congress/v1/"
@@ -10,9 +11,7 @@ SEARCH_BILL = "bills/search.json?"
 QUERY = "&query={}"
 OFFESET_S = "&offset={}"
 OFFSET_N = 10000
-# use `.format()` to add params to the query
-    # leave in paranthesis to search as one term 
-    # or dont include parenthesis to search multiple terms
+
 def read_file (file_path):
     with open(file_path, 'r') as file:
         return file.read()
@@ -20,13 +19,11 @@ def read_file (file_path):
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-def hello():
-
+def ayo():
     return "theres nothing here bruh"
 
-@app.route('/search/<string:query>', methods=['GET'])
+@app.route('/search_bill/<string:query>', methods=['GET'])
 def search(query):
-    # perform a search for the data from .json file
     # using vector
     return query 
 
@@ -34,6 +31,21 @@ def search(query):
 def get_image(query):
     return image_search(query)
 
+@app.route('/get_news/<query>', methods=['GET'])
+async def get_news(query):
+    response = requests.get("https://api.worldnewsapi.com/search-news", params={
+        "api-key": "d854699510c743a9b860bab2675b1b4d",
+        "text": query, 
+        "source-countries": "us",
+        "language": "en",
+        "sort": "publish-time",
+        "sort-direction": "DESC",
+        })
+    return response.json()
+
+@app.route('/get_contact_info', methods=['GET'])
+def get_contact_info():
+    return "contact info"
 
 if __name__ == '__main__':
     app.run(debug=False)
