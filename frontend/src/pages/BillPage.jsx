f6067869a9af86873bf5ec4dec803b3bbeefe048
Mahdi <mahditanzim2@gmail.com>
2024-02-03 20:51:19 -0500

    .

--- frontend/src/pages/BillPage.jsx
+++ frontend/src/pages/BillPage.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import Nav from '../components/Nav';
-import { Chrono } from "react-chrono";
 import axios from 'axios';
 
 
